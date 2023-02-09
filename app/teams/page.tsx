ab8fccadf0aca091879684904723ceb8331122ab
jayzumen <reinhardtjanniklas96@gmail.com>
2023-02-09 13:02:46 +0100

    changed fetching for teams

--- app/teams/page.tsx
+++ app/teams/page.tsx
@@ -1,37 +1,10 @@
-import { collection, DocumentData, getDocs } from "firebase/firestore";
-import { db } from "./../firebase";
 import TeamsDisplay from "./TeamsDisplay";
 
-async function getTeams() {
-  const teamsRef = collection(db, "Teams");
-  const querySnapshot = await getDocs(teamsRef);
-  const data: DocumentData[] = [];
-  const teamData: DocumentData[] = [];
-  querySnapshot.forEach(async (doc) => {
-    const pokemonTeamRef = collection(db, "Teams", doc.id, "pokemonTeam");
-    teamData.push(
-      getDocs(pokemonTeamRef).then((pokemonTeamSnapshot) => {
-        const pokemonTeamData: DocumentData = [];
-        pokemonTeamSnapshot.forEach((pokemonDoc) => {
-          pokemonTeamData.push(pokemonDoc.data());
-        });
-        data.push({
-          userId: doc.id,
-          pokemonTeam: pokemonTeamData,
-        });
-      })
-    );
-  });
-  await Promise.all(teamData);
-  return data;
-}
-
 export default async function TeamsPage() {
-  const teams = await getTeams();
   return (
-    <main className="px-4 pt-32 pb-4">
+    <main className="min-h-screen px-4 pt-32 pb-4">
       <h1 className="text-4xl font-semibold underline">Teams</h1>
-      <TeamsDisplay teams={teams} />
+      <TeamsDisplay />
     </main>
   );
 }
